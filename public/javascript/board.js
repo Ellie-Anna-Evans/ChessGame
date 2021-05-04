60066757c9a74708c32439b136888e9ac79b384a
evansdr1 <evansdr@oregonstate.edu>
2021-05-03 19:59:50 -0700

    Added in board listener function, added comments for potential logic in the board functions.

--- public/javascript/board.js
+++ public/javascript/board.js
@@ -1,7 +1,23 @@
-function swapPieces(row1, column1, row2, column2){
+var board = document.getElementsByClassName("Board").children;
 
+function boardListeners(){
+    var i;
+    for(i = 0; i < board.length; i++){
+       var boardSpace = board[i];
+       boardSpace.addEventListener("click", clickHandler);
+    }
+}
+
+function clickHandler(){
+    //Need to figure out how to handle this so it intercepts the second click properly.
+    //Perhaps remove all listeners and then send it to movePiece to handle specific piece movement
+}
+
+function movePiece(row1, column1, row2, column2, type){
+    //Will move piece based on piece type and parameters. Might have this function add the listeners 
+    //back in for what spots the piece can move to.
 }
 
 function makeEmpty(row, column){
-
+    //This function is for when a piece is taken and removed from the board
 }
