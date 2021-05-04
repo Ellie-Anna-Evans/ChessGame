7e093392917c71cf58412f2c218f8f870105e60d
evansdr1 <evansdr@oregonstate.edu>
2021-05-03 21:36:08 -0700

    Added classes for board and spaces. Board still needs a setup function, but I don't currently have an eleagant solution in mind.

--- public/javascript/board.js
+++ public/javascript/board.js
@@ -1,9 +1,46 @@
-var board = document.getElementsByClassName("Board").children;
+class Space{
+    constructor(piece, row, column){
+        this.piece = piece;
+        this.row = row;
+        this.column = column;
+    }
+
+    get piece(){
+        return this.piece;
+    }
+
+    get location(){
+        return (this.row, this.column);
+    }
+}
+
+class Board{
+    constructor(){
+        this.board = [];
+    }
+
+    //can't think of an eleagant solution to setting up the board
+    //will probably just be hackish for now and just declare all the unique spaces,
+    //and loop through the non-unique ones.
+    setupBoard(){
+
+        this.board.push();
+        
+        var i;
+        for(i = 0; i < 8; i++){
+            
+        }
+    }
+
+}
+
+
+var boardVis = document.getElementsByClassName("Board").children;
 
 function boardListeners(){
     var i;
-    for(i = 0; i < board.length; i++){
-       var boardSpace = board[i];
+    for(i = 0; i < boardVis.length; i++){
+       var boardSpace = boardVis[i];
        boardSpace.addEventListener("click", clickHandler);
     }
 }
@@ -11,6 +48,16 @@ function boardListeners(){
 function clickHandler(){
     //Need to figure out how to handle this so it intercepts the second click properly.
     //Perhaps remove all listeners and then send it to movePiece to handle specific piece movement
+    var i;
+    for(i = 0; i < boardVis.length; i++){
+        var boardSpace = boardVis[i];
+        boardSpace.removeEventListener("click", clickHandler);
+    }
+    
+}
+
+function secondClick(){
+
 }
 
 function movePiece(row1, column1, row2, column2, type){
